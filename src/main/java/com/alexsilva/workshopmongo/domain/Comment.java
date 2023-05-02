e9f8c50cd100c74aaef197582b625b6b3a2349ee
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-05-02 13:35:10 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/alexsilva/workshopmongo/domain/Comment.java
@@ -0,0 +1,88 @@
+package com.alexsilva.workshopmongo.domain;
+
+import java.time.Instant;
+import java.util.Objects;
+
+
+public class Comment {
+	
+	private Long id;
+	private String text;
+	private Instant date;
+	
+	private Post posts;
+	private User author;
+	
+	public Comment() {
+	}
+
+	public Comment(Long id, String text, Instant date, Post posts, User author) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.date = date;
+		this.posts = posts;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Post getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Post posts) {
+		this.posts = posts;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/alexsilva/workshopmongo/domain/User.java
@@ -0,0 +1,78 @@
+package com.alexsilva.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String email;
+	
+	private Set<Post> posts = new HashSet<>();
+	
+	public User() {
+	}
+
+	public User(Long id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+
+	
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
